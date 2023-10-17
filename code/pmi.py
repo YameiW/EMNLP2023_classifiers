32335d24ab8c500e8978919bb7544e75c36287f2
Yamei <wangyamei01.11@gmail.com>
2023-10-17 13:45:39 -0400

    complete pmi.py

--- code/pmi.py
+++ code/pmi.py
@@ -1,4 +1,7 @@
+# this script is run on the cluster where the parsed conllu files are stored
 import os
+import pandas as pd
+import pickle
 from collections import defaultdict
 import pyconll
 from joblib import Parallel, delayed
@@ -32,3 +35,20 @@ def process_files_in_folder(folder_path, func, *args,n_jobs=-1):
     file_paths = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if filename.endswith('.conllu')]
     results = Parallel(n_jobs=n_jobs)(delayed(func)(file_path,*args) for file_path in file_paths)
     return results
+
+pair_df1 = pd.read_csv("./clf_noun_structure.csv")
+pair_df2 = pd.read_csv("./clf_mod_noun_structure.csv")
+
+target_nouns1 = pd.concat([pair_df1['noun1'], pair_df1['noun2']]).unique()
+target_nouns2 = pd.concat([pair_df2['noun1'], pair_df2['noun2']]).unique() 
+
+folder_path = '/home/ywang78/scratch/conllu/leipzig_conllu'
+
+occurrence_ls1 =  process_files_in_folder(folder_path, occurrence, target_nouns1)
+occurrence_ls2 =  process_files_in_folder(folder_path, occurrence, target_nouns2)
+
+with open('../data/clf_noun_pmi.pkl','wb') as file:
+    pickle.dump(occurrence_ls1, file) 
+
+with open('../data/clf_mod_noun_pmi.pkl','wb') as file:
+    pickle.dump(occurrence_ls2, file)
